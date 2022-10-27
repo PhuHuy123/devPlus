3cbf03e9f39fc5530bb8aeacbdbbde314178e530
hieunqpd04773 <hieunqpd04773@fpt.edu.vn>
2022-10-27 15:51:58 +0700

    Hieu Up load

--- js/myFunctions.js
+++ js/myFunctions.js
@@ -3,18 +3,37 @@ let btnBackTop=document.getElementById('btnBackTop');
 window.onscroll=function(){
 scrollFunction();
 }
-function scrollFunction(){
-if(document.body.scrollTop > 600 || document.documentElement.scrollTop > 600){
-    btnBackTop.style.display='block';
-}
-else{
-    btnBackTop.style.display='none';
-}
-}
+// function scrollFunction(){
+
+// }
 
 function topFunction(){
 document.body.scrollTop=0;
 document.documentElement.scrollTop=0
+}
+function scrollFunction() {
+  if(document.body.scrollTop > 600 || document.documentElement.scrollTop > 600){
+    btnBackTop.style.display='block';
+  }
+  else{
+      btnBackTop.style.display='none';
+  }
+
+
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-90px";
+  }
+  if (
+    document.body.scrollTop == 0 ||
+    document.documentElement.scrollTop == 0
+  ) {
+    document.getElementById("header").style.top = "0px";
+  }
 }
 
 // Loader
@@ -22,6 +41,7 @@ var myVar;
 
 function myFunction() {
   myVar = setTimeout(showPage, 1500);
+  document.getElementsByClassName
 }
 
 function showPage() {
