03312277f898fe22bc3fcd95a95d966e5452c79b
anh1007 <93590958+anh1007@users.noreply.github.com>
2022-10-27 18:00:13 +0700

    Fix header (#7)
    * header-footer
    
    * Hieu UpLoad
    
    * done popular & admision
    
    done popular & admission
    
    fix bug
    
    * fix modal
    
    Co-authored-by: PhuHuy123 <nguyenphuhuy180920@gmail.com>
    Co-authored-by: hieunqpd04773 <hieunqpd04773@fpt.edu.vn>

---
+++ js/myFunctions.js
@@ -0,0 +1,32 @@
+// BTN BACK TOP
+let btnBackTop = document.getElementById("btnBackTop");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    btnBackTop.style.display = "block";
+  } else {
+    btnBackTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Loader
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+}
+AOS.init();
